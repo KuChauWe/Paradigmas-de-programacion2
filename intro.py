74ae3a56e97a17a35abee56ca9705002f6aec011
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2023-03-25 02:56:16 -0600

    Llegué a mensajes en pantalla

--- intro.py
+++ intro.py
@@ -6,3 +6,20 @@ print (2+3)
 print (2*3)
 print (2/3)
 print (2**10)
+print (2**0.5) #raíz cuadrada
+print (10%2) #residuo
+print (10%0.1) #solo de python 
+
+#==============================================
+# Para saber el tipo de objeto qque se usa type
+#==============================================
+t=0
+print(type(t)) #entero
+t=3.6
+print(type(t)) #real (flotante)
+t=True
+print(type(t)) #booleano (bool)
+
+#=====================
+# Mensajes en pantalla
+#=====================
