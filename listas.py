5dcea7c4edf9d2992dd986d0e22b29a452d2cbf2
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2023-03-26 12:45:09 -0600

    Completo, solo con unas cuantas dudas ඞ

--- listas.py
+++ listas.py
@@ -28,3 +28,41 @@ nums.append(62)
 nums.append(61)
 print(nums)
 
+#=============================
+# Quitar elementos de la lista
+#=============================
+nums.remove(61)
+print (nums)
+
+#=============================
+# Quitar elemento con índice i
+#=============================
+i = 61
+del nums[i]
+print (nums)
+
+#================
+# Borrar la lista
+#================
+del nums
+
+#=============
+# Sumar listas
+#=============
+L1 = [1,2,3]
+L2 = [4,5,6]
+print (L1+L2)
+
+#===============
+# Llenado a mano
+#===============
+potencial = []
+for i in range(0,10000):
+    potencial.append(float(i))
+print (potencial[100])
+
+#===============================
+# Generar una tupla con la lista
+#===============================
+potencial = tuple(potencial)
+print(potencial[100])
