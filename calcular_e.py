726e3488a9e38d060df51222af3b752832c0e710
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2023-04-03 12:43:55 -0600

    Correción de la escritura de los booleanos

--- calcular_e.py
+++ calcular_e.py
@@ -7,15 +7,15 @@
 #==============================
 n = 200
 x = -100.0
-flag= false
+flag= False
 if x < 0:
-    flag = true
+    flag = True
     x = -x
 s = 1.0
 for i in range(n,0,-1):
      s *= x/float(i)
      s += 1.0
-if flag == true 
+if flag == True 
  s = 1/s
  print (s)
 
