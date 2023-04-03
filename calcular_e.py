7554fa2600b75358b4c96b95d2def795e349194e
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2023-04-03 12:21:33 -0600

    Primer intento de calcular e

---
+++ calcular_e.py
@@ -0,0 +1,15 @@
+#==============================
+# Algoritmo 1
+#==============================
+# Serie exponencial
+# Factorización de x
+# Negativos con función inversa
+#==============================
+n = 200
+x = -100.0
+flag= false
+if x < 0:
+    flag = true
+    x = -x
+
+
