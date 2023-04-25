e60bfcccdada34df80afb71fdf605d03255c9a9b
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2023-04-25 01:34:33 -0600

    Listo, vivo y coleandoඞ

--- loops.py
+++ loops.py
@@ -4,13 +4,13 @@
 precio = 50
 # Si esto...
 if precio < 50:
-  print ("El precio es menor que 50")
+  print("El precio es menor que 50")
 # Si no... si esto otro...
 elif precio > 50:
-  print ("El precio es mayor a 50")
+  print("El precio es mayor a 50")
 # Ya si todo demás no embona...
 else:
-    print ("El precio es 50")
+    print("El precio es 50")
 
 precio = 50
 cantidad = 5
@@ -21,20 +21,20 @@ total = precio*cantidad
 #=======================
 if total > 100:
     if total > 500:
-        print ("Total es mayor que 500")
+        print("Total es mayor que 500")
     else:
         if total < 500 and total > 400:
-            print ("Total es menor que 500 pero mayor que 400")
+            print("Total es menor que 500 pero mayor que 400")
         elif total < 500 and total > 300:
-            print ("Total entre 300 y 500")
+            print("Total entre 300 y 500")
         else:
-            print ("Total entre 100 y 300")
+            print("Total entre 100 y 300")
 
 # Condicional de igualdad (==)
 elif total == 100:
-    print ("Total es 100")
+    print("Total es 100")
 else:
-    print ("Total menor que 100")
+    print("Total menor que 100")
 
 #=============================================
 # Contador mientras la condición sea verdadera
@@ -42,12 +42,12 @@ else:
 num = 0
 while num < 5:
     num = num + 1
-    print ('num = ',num)
+    print('num = ',num)
 
 num = 0
 while num < 5:
     num += 1                # num += 1 es lo mismo que num = num + 1
-    print ('num = ', num)   
+    print('num = ', num)   
     if num == 3:             # condición antes de sallir del bucle
         break 
 
@@ -57,20 +57,20 @@ while num < 5:
     if num > 3:
         continue            # evitar lo que sigue, continuar con las iteracines es
 
-    print ('num = ',num)
+    print('num = ',num)
 
 #==================
 # Bucle sobre lista
 #==================
 nums = [10,20,30,40,50]
 for i in nums:
-    print (i)
+    print(i)
 
 #=======================
 # Bulcle sobre un string
 #=======================
 for char in 'Hello':
-    print (char)
+    print(char)
 
 #===========================
 # Bucle sobre un diccionario
@@ -78,7 +78,7 @@ for char in 'Hello':
 #===========================
 numNames = {1:'One',2:'Two',3:'Three'}
 for pair in numNames.items():
-    print (pair)
+    print(pair)
 
 #========================
 # Bucle sobre diccionario
