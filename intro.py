6ecbaa4340fde1601abd86eeb358df918a7da434
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2023-04-25 01:29:18 -0600

    Listo, vivo y coleandoඞ

--- intro.py
+++ intro.py
@@ -2,13 +2,13 @@
 #=============================
 # Operaciones básicas juasjuas
 #=============================
-print (2+3)
-print (2*3)
-print (2/3)
-print (2**10)
-print (2**0.5) #raíz cuadrada
-print (10%2) #residuo
-print (10%0.1) #solo de python 
+print(2+3)
+print(2*3)
+print(2/3)
+print(2**10)
+print(2**0.5) #raíz cuadrada
+print(10%2) #residuo
+print(10%0.1) #solo de python 
 
 #============================================= 
 # Para saber el tipo de objeto que se usa type 
@@ -23,11 +23,11 @@ print(type(t)) #booleano (bool)
 #=====================
 # Mensajes en pantalla
 #=====================
-print ("Este es un comentario en python. ", "Este es otro enunciado")
-print ('id: ',1)
-print ('First Name: ', 'Doo') 
-print ('Last Name: ', 'Mentio')
-print ("Vamos a sumar esto" + " con esto otro")
+print("Este es un comentario en python. ", "Este es otro enunciado")
+print('id: ',1)
+print('First Name: ', 'Doo') 
+print('Last Name: ', 'Mentio')
+print("Vamos a sumar esto" + " con esto otro")
 
 #==============================================
 # Continuar una instruccion en varios renglones
@@ -40,7 +40,7 @@ if 100 > 99 and \
 #======================================
 # Comandos diferentes en la misma línea
 #======================================
-print ("Hola "); print ("tu!!") #Se considera mala práctica o.O
+print("Hola "); print("tu!!") #Se considera mala práctica o.O
 
 #===============================================
 # Usando paréntesis redondos, cuadrados o llaves
@@ -59,26 +59,26 @@ print(matriz)
 # Indentación estricta para procesos dependientes de : (dos puntos)
 #==================================================================
 if 10 > 5:
-  print ("diez es mayor que cinco") 
-  print ("otra indentación")
+  print("diez es mayor que cinco") 
+  print("otra indentación")
 for i in list:
-  print (i)
-print ("Ok")
+  print(i)
+print("Ok")
 if 10 > 5:
-  print ("verdadero")
+  print("verdadero")
   if 10 < 20:
-    print ("verdadero")
+    print("verdadero")
 elif 5 > 3: # Comienza segundo condicional
-  print ("esto no se imrpimirá")
+  print("esto no se imrpimirá")
 else:
-  print ("aquí nunca llega")
+  print("aquí nunca llega")
 
 #==========       
 # Funciones
 #==========
 def say_hello(name):
-    print ("Hello", name)
-    print ("Welcome to Python Tutorials")
+    print("Hello", name)
+    print("Welcome to Python Tutorials")
 
 say_hello("Hugo")
 
@@ -92,20 +92,20 @@ say_hello("Hugo")
 #====================================================
 # Input permite obtener datos del usuario en prompter
 #====================================================
-nombre = input ("Dame tu nombre: ")
-print ("Felicidades, cumples años en: ", nombre)
+nombre = input("Dame tu nombre: ")
+print("Felicidades, cumples años en: ", nombre)
 
 #=======================================
 # Los enteros son de precisión ilimitada
 #=======================================
 y = 500000000000000000000000000000000
-print (y)
+print(y)
 
 #==================================================================
 # Se pueden delimitar números con algún guión bajo pero no con coma
 #==================================================================
 y = 5_000_000
-print (y)
+print(y)
 
 #===================================================
 # La función int() cambia strings y floats a enteros
@@ -119,13 +119,13 @@ type(numero)
 # 1.2 x 10^{-9}                  
 #==============
 y = 1.2E-09
-print (y)
+print(y)
 
 #========================================================
 # La función float() convierte strings y enteros a floats
 #========================================================
 y = float("14.3")
-print (y)
+print(y)
 
 #===================================================
 # Los complejos se escriben con la raíz de menos uno
@@ -151,13 +151,13 @@ print(round(3.14159,4))
 #=========================
 parrafo = """En el bosque de la china
  la chinita se perdió """
-print (parrafo)
+print(parrafo)
 
 #==============================================
 # La función len() obtiene el tamaño del string
 #==============================================
 n = len(parrafo)
-print (n)
+print(n)
 
 #============================================================
 # Las letras en un string ocupan lugares como en un arreglo
