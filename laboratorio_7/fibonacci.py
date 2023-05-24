baab4162f03bf00ce155a7ffe8bf4700db78779c
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2023-05-24 13:51:25 -0600

    fibonacci terminado y corriendo

--- laboratorio_7/fibonacci.py
+++ laboratorio_7/fibonacci.py
@@ -7,13 +7,13 @@
 #==============================
 from functools import lru_cache
 
-def feibonacci_lento(n):
+def fibonacci_lento(n):
     if n==1:
         return 1
     if n==2:
         return 2
     if n>2:
-        return fibonacci_lento(n-1)+fibonaccio_lento(n-2)
+        return fibonacci_lento(n-1)+fibonacci_lento(n-2)
 
 for i in range(1,36):
     print(str(i)+":",fibonacci_lento(i))
@@ -27,13 +27,13 @@ for i in range(1,36):
 # Conjunto de fibonaccis
 #=======================
 fibonaccis = {}
-def fibonacci(n)
+def fibonacci(n):
 
     #=======================================
     # Revisa si ya existe y regresa el valor
     #=======================================
-    if n in fibonnaccis:
-        return fibonnaccis[n]
+    if n in fibonaccis:
+        return fibonaccis[n]
 
     if n==1:
         valor = 1
@@ -55,4 +55,19 @@ for i in range(1,10001):
 # Uso de decoradores para memoización
 # maxsize: es cuantos anteriores guarde
 #======================================
+@lru_cache(maxsize=3)
+def nfibonacci(n):
+    if type(n) != int:
+        raise TypeError("n debe ser entero positivo")
+    if n<1:
+        raise ValueError("n debe ser entero positivo")
+    if n==1:
+        return 1
+    elif n==2:
+        return 1
+    elif n>2:
+        return nfibonacci(n-1)+nfibonacci(n-2)
+
+for i in range(1,10001):
+    print(str(i)+":",nfibonacci(i))
 
