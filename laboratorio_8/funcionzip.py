fde99ff55de258bbf94a9ae1bc58f812713baeb4
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2023-05-25 00:44:20 -0600

    Labo 8.0 completado 0.n

--- laboratorio_8/funcionzip.py
+++ laboratorio_8/funcionzip.py
@@ -13,3 +13,21 @@ print(list(zip(mi_lista,tu_lista,su_lista)))
 una_lista = ["a","b","c","b","d","m","n","n"]
 duplicados = set([x for x in una_lista if una_lista.count(x)>1])
 print(duplicados)
+
+# Expresión generadora
+cuadrados = (x*x for x in range(5))
+print(next(cuadrados))
+print(next(cuadrados))
+print(next(cuadrados))
+print(next(cuadrados))
+print(next(cuadrados))
+
+# Pasar una función generadora
+import math
+print(sum(x*x for x in range(5)))
+
+# Lista  de comprehesión pasada como función
+numeros_pares = [x for x in range(21) if x%2 == 0]
+print([x for x in range(21) if x%2 == 0])
+print(numeros_pares)
+
