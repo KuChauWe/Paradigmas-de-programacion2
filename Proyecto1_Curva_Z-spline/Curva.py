753916f3e60972e252cb0d74428ed89831860ba5
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2023-06-26 11:49:43 -0600

    Avance en la tupla, me duele la cabeza

---
+++ Proyecto1_Curva_Z-spline/Curva.py
@@ -0,0 +1,48 @@
+#===========================================
+# Cálculo de curva Z-spline en n dimensiones
+#===========================================
+# Julián Sagredo ESFM IPN 2023
+#=============================
+import numpy as np
+
+#============
+# Clase Curva
+#============
+class Curva:
+    
+    """==================================================
+        Curva contruye la curva que pasa por los puntos x
+       ==================================================
+       Parámetros: x coordenadas ordenadas((x1),(x2),...)
+                   f propiedades (f1(x),f2(x),...)
+                   dim dimensiones
+       ==================================================
+    """
+    #===========
+    # Contructor
+    #===========
+    def __init__(s, x:float=[], dim:int=3)
+    
+        s.x = np.array(x,dtype=np.float64)
+        s.dim = dim
+        s.n:np.int32 = int(len(s.x)/s.dim)  # Número de puntos
+        s.l = []                            # Longitud sobre la curva
+        s.lista_de_puntos()
+        s.longitud()
+
+    #================
+    # Lista de puntos
+    #================
+    def lista_de_puntos(s) -> str:
+
+        print(f"Número de puntos = {str(s.n)}")
+
+        # Formato  de datos
+        s.formato = ""
+        for j in range(s.din):
+            s.formato += "%15.8e"
+        s.formato += "\n"
+
+        # Tupla de variables a imprimir
+        for i in range(0,s.n):
+            s.tup
