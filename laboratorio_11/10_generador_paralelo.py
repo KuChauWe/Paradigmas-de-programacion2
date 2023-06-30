2da16320315985917446279449a5392e650f34d0
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2023-06-30 16:42:44 -0600

    Fuera terminado

--- laboratorio_11/10_generador_paralelo.py
+++ laboratorio_11/10_generador_paralelo.py
@@ -5,20 +5,20 @@ import os
 N: int = 10
 
 def generador(N: float ) -> None:
-    semilla:float = random.uniform(-1 , 1)
+    semilla:float = random.uniform(-1, 1)
     print("La semilla es: " , semilla)
     random.seed(semilla)
     for i in range(N):
-        x:float = random.uniform(-1 , 1)
-        y:float = random.uniform(-1 , 1)
-        print("x = " , x , "\t y= " , y)
+        x:float = random.uniform(-1, 1)
+        y:float = random.uniform(-1, 1)
+        print("x = " , x , "\t y= ", y)
 
 
 procesos=[]
 cpus = os.cpu_count()
 print("Porcesadores = " , cpus)
 for i in range(cpus):
-    procesos.append(Process(target = generador , args=(N , )))
+    procesos.append(Process(target = generador, args=(N, )))
 
 #==================================
 # Comienza los procesos en paralelo
--- laboratorio_11/11_montecarlo.py
+++ laboratorio_11/11_montecarlo.py
@@ -3,13 +3,13 @@ import random
 import os
 
 def montecarlo(N:float) -> int:
-    semilla:float = random.uniform(-1 , 1)
+    semilla:float = random.uniform(-1, 1)
     random.seed(semilla)
     dentro:int = 0
 
     for i in range(N):
-        x:float = random.uniform(-1 , 1)
-        y:float = random.uniform(-1 , 1)
+        x:float = random.uniform(-1, 1)
+        y:float = random.uniform(-1, 1)
         if (x*x + y*y) < 1.0:
             dentro += 1
     return dentro
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     # El objeto grupo tiene método map para repetir tarea
     #====================================================
 
-    results = Pool(cpus).map(montecarlo , arg)
-    print("Número de tiros = " , cpus*N)
-    print("Número de aciertos " , results)
-    print("Aproximación de pi = " , 4*sum(results)/(cpus*N))
+    results = Pool(cpus).map(montecarlo, arg)
+    print("Número de tiros = ", cpus*N)
+    print("Número de aciertos ", results)
+    print("Aproximación de pi = ", 4*sum(results)/(cpus*N))
