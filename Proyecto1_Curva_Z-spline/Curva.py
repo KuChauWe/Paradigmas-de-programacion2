45a3927cfe911dc59747f6dd7f7ab1c2ed77be57
KuChauWe <gomez.ocampo.hugo.1@gmail.com>
2023-06-29 21:52:35 -0600

    Más sintáxis

--- Proyecto1_Curva_Z-spline/Curva.py
+++ Proyecto1_Curva_Z-spline/Curva.py
@@ -21,7 +21,7 @@ class Curva:
     # ===========
     # Constructor
     # ===========
-    def __init__(s, x: float = [], dim: int = 3):
+    def __init__(s,x:float=[], dim:int=3):
 
         s.x = np.array(x, dtype=np.float64)
         s.dim = dim
@@ -35,7 +35,7 @@ class Curva:
     # ===============
     def lista_de_puntos(s) -> str:
 
-        print(f"Numero de puntos = {str(s.n)}")
+        print(f"Numero de puntos={str(s.n)}")
 
         # Formato de datos
         s.formato = ""
@@ -47,8 +47,8 @@ class Curva:
         for i in range(0, s.n):
             s.tup = (s.x[i],)
             for ii in range(1, s.dim):
-                s.tup = s.tup + (s.x[i + ii * s.n],)
-            print(s.formato % s.tup)
+                s.tup = s.tup + (s.x[i+ii*s.n],)
+            print(s.formato%s.tup)
 
     # ======================
     # Longitud punto a punto
@@ -59,24 +59,25 @@ class Curva:
             ip1 = i + 1
             if i == s.n - 1:
                 ip1 = 0
-            d: np.float64 = (s.x[ip1] - s.x[i]) ** 2
+            d: np.float64 = (s.x[ip1]-s.x[i]) ** 2
             for j in range(1, s.dim):
-                d += (s.x[ip1 + j * s.n] - s.x[i + j ** s.n]) ** 2
-                t += d ** 0.5
+                d += (s.x[ip1+j*s.n]-s.x[i+j**s.n])**2
+                t += d**0.5
                 s.l.append(t)
             s.L = t
-            s.dx = t / float(s.n)
+            s.dx = t/float(s.n)
 
     # =============
     # Interpolacion
     # =============
-    def interpolacion(s, p: int = 0, r: float = 0.0) -> list:
-        """r es el parametro sobre la curva [0,1)
+    def interpolacion(s,p:int=0, r:float=0.0) -> list:
+        """
+           r es el parametro sobre la curva [0,1)
            p es la suavidad de la curva
         """
         rdx: np.float64 = 1.0 / s.dx
         xi: float = []
-        i: np.int32 = int(r * s.L * rdx)
+        i: np.int32 = int(r*s.L * rdx)
         a: np.float64 = r * s.L * rdx - float(i)  # Distancia normalizada
 
         # ========================
